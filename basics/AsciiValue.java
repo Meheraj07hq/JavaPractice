d3975e5df46688c174e9e6d2e053c78fb92b94a5
Munsi Meheraj <munsimeheraj01@gmail.com>
2026-09-22 23:21:42 +0530

    AsciiValue

---
+++ basics/AsciiValue.java
@@ -0,0 +1,10 @@
+public class AsciiValue {
+    public static void main(String[] args) {
+
+        char character = 'a';
+
+        int ascii = character;
+
+        System.out.println("ASCII value of " + character + " = " + ascii);
+    }
+}
