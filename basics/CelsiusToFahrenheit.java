39bdd6ea3191c22b963776fba5a226a13d2a2e0c
Munsi Meheraj <munsimeheraj01@gmail.com>
2026-09-22 23:15:22 +0530

    Celsius to fahrenheit

---
+++ basics/CelsiusToFahrenheit.java
@@ -0,0 +1,10 @@
+public class CelsiusToFahrenheit {
+    public static void main(String[] args) {
+
+        double celsius = 31;
+
+        double fahrenheit = (celsius * 9 / 5) + 32;
+
+        System.out.println(celsius + " Celsius = " + fahrenheit + " Fahrenheit");
+    }
+}
