529c5ec6d1eeb0aa98eed40a7a7e844091bb1b35
Munsi Meheraj <munsimeheraj01@gmail.com>
2026-09-23 22:18:07 +0530

    AreaOfCircle

---
+++ basics/AreaOfCircle.java
@@ -0,0 +1,10 @@
+public class AreaOfCircle {
+    public static void main(String[] args) {
+
+        double radius = 5;
+
+        double area = Math.PI * radius * radius;
+
+        System.out.println("Area of Circle = " + area);
+    }
+}
