0f9a8ed86314d59654e919fb02a0ec41b011d6cc
Munsi Meheraj <munsimeheraj01@gmail.com>
2026-09-25 22:57:25 +0530

    ArmstrongNumber

---
+++ basics/ArmstrongNumber.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class Armstrong {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int original = n;
+        int temp = n;
+        int digits = 0;
+        int sum = 0;
+
+        // Count number of digits
+        while (temp > 0) {
+            digits++;
+            temp = temp / 10;
+        }
+
+        temp = n;
+
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum = sum + (int)Math.pow(digit, digits);
+            temp = temp / 10;
+        }
+
+        if (sum == original)
+            System.out.println("Armstrong number");
+        else
+            System.out.println("Not an Armstrong number");
+    }
+}
